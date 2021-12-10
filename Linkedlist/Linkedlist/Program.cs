474de029008162f66b0b508cf348b083ab8c8bc1
Rajangane <pratiksharajangane7@gmail.com>
2021-12-10 11:34:52 +0530

    [Pratiksha] ADD : Inserting 40 after the 30 in the linkedlist

--- Linkedlist/Linkedlist/Program.cs
+++ Linkedlist/Linkedlist/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("************** Welcome To Linkedlist***************");
 using Linkedlist;
-Console.Write("Select Number:\n1)Add elements\n2)Append\n3)Insert\n4)RemoveFirstElement\n5)RemoveLastElement\n6)Search");
+Console.Write("Select Number:\n1)Add elements\n2)Append\n3)Insert\n4)RemoveFirstElement\n5)RemoveLastElement\n6)Search\n7)Insert40");
 int option = Convert.ToInt32(Console.ReadLine());
 Linked_List<int> list = new Linked_List<int>();//creating list
 switch (option)
@@ -47,6 +47,14 @@ switch (option)
         list.Add(30);
         list.Add(70);
         Console.WriteLine(list.Search(30));
+        break;
+    case 7:
+        list.Add(56);
+        list.Add(30);
+        list.Add(70);
+        list.Insert(3, 40);//in the position of 3 insert 40
+        Console.WriteLine("Linked list elements after inserting :");
+        list.Display();
         break;
 
     default:
